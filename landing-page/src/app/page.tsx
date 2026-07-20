e99b83c51f8df9823436401809d490fc417ee1d6
Pushkar Raj <pushkarcodes10@gmail.com>
2026-07-21 01:24:23 +0530

    Add new Next.js and Tailwind CSS landing-page project

---
+++ landing-page/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Hero } from "../components/hero";
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-neutral-900 flex justify-center">
+      <Hero />
+    </div>
+  );
+}
---
+++ landing-page/src/components/hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div className='my-40'>
+      <h1 className="bg-linear-to-br from-white to-zinc-500 bg-clip-text text-center text-5xl/[1.07] font-bold tracking-tight text-transparent md:text-7xl/[1.07]">
+        Unleash the power of <br /> intuitive finance
+      </h1>
+      <p className='text-white mt-5 text-md text-center'>
+        Say goodbye to the outdated financial tools. Every small business owner,    <br />
+        regardless of the background, can now manage their business like a pro.     <br />
+        Simple. Intuitive. And never boring.
+      </p>
+    </div>
+  );
+}
